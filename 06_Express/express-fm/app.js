ddcf9a6576a34135f4f88eb5768a81cabac44339
weihy <971973918@qq.com>
2023-10-10 16:16:31 +0800

    实现插入用户数据

--- 06_Express/express-fm/app.js
+++ 06_Express/express-fm/app.js
@@ -3,17 +3,9 @@ const fs = require("fs");
 const { promisify } = require("util");
 // 将fs.readFile promise化
 const readFile = promisify(fs.readFile);
+const writeFlile = promisify(fs.writeFile);
 const app = express();
-// app.get("/", function (req, res) {
-//   fs.readFile("./db.json", "utf8", (err, data) => {
-//     if (!err) {
-//       var back = JSON.parse(data);
-//       res.send(back.users);
-//     } else {
-//       res.status(500).json({ err });
-//     }
-//   });
-// });
+
 // 避免回调地狱，将readFile Promise化
 // 应用urlencoded中间件，用于解析application/x-www-form-urlencoded格式的请求体数据
 app.use(express.urlencoded());
@@ -29,8 +21,35 @@ app.get("/", async function (req, res) {
   }
 });
 app.post("/", async (req, res) => {
-  console.log("请求头信息:", req.headers);
+  //   console.log("请求头信息:", req.headers);
   console.log("req", req.body);
+  let body = req.body;
+  if (body == {}) {
+    res.status(403).json({
+      error: "缺少用户信息",
+    });
+  }
+  let dbData = await readFile("./db.json", "utf8");
+  let dbDataObj = JSON.parse(dbData);
+  let userObj = dbDataObj.users;
+  let newId = userObj[userObj.length - 1].id + 1;
+  //   将新id放入body中
+  body.id = newId;
+  userObj.push(body);
+  console.log("新增数据后的userObj", dbDataObj);
+  //   写入本地db.json
+  try {
+    let writeRes = await writeFlile("./db.json", JSON.stringify(dbDataObj));
+    if (!writeRes) {
+      res.status(200).send({
+        msg: "写入成功",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error,
+    });
+  }
 });
 
 app.listen(3000, () => {
